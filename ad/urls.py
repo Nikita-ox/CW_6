fc65f7490d0cfeef36ff6156229daed3d74e64ae
NIK <nikita1z@mail.ru>
2022-09-30 21:46:53 +0300

    Initial commit

---
+++ ad/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework import routers
+
+# from ad import viewsets
+# from ad.views import CommentViewSet
+
+from ad import views
+from ad.views import CommentViewSet
+
+comments_router = routers.SimpleRouter()
+comments_router.register('comments', CommentViewSet)
+
+urlpatterns = [
+	path('me/', views.MyAdsView.as_view(), name='my_ads'),
+	path('<int:ad_pk>/', include(comments_router.urls), name='comments'),
+]
